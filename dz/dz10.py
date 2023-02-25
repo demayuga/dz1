b026d0876c6803e42a4258d4e3e8161419fd3232
demayuga <123291766+demayuga@users.noreply.github.com>
2023-02-25 19:42:22 +0200

    Delete dz10.py

--- dz/dz10.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-connection = sqlite3.connect('basetemp.sl3', 5)
-cursor = connection.cursor()
-
-cursor.execute("INSERT INTO temperature(temperature, date_and_time) VALUES ('+2', '24.02.23:17.00');")
-
-connection.commit()
-connection.close()
